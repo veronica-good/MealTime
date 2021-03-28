3e515ad3327df105465f84ebecee8f0a44f0693f
Veronica <veronica.gudova@gmail.com>
2021-03-28 14:13:11 -0700

    installed ui libraries

--- mealtime_client/src/requests.js
+++ mealtime_client/src/requests.js
@@ -3,8 +3,12 @@ const BASE_URL = 'http://localhost:3000/api/v1';
 export const Food={
     index(){
         return fetch(`${BASE_URL}/foods`)
-            .then(res=>{
-                return res.json();
-            })
+            .then(res => res.json())
+            .catch(console.error)
+    },
+    show(id){
+        return fetch(`${BASE_URL}/foods/${id}`)
+        .then(res => res.json())
+        .catch(console.error)
     }
 }
